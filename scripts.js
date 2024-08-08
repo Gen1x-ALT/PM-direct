7e8b122810d41e6fd447753f00049440112575d6
Gen1x-ALT <153012779+Gen1x-ALT@users.noreply.github.com>
2024-08-08 03:53:31 +0000

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// JavaScript to handle the menu toggle in mobile view
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('nav').classList.toggle('nav-active');
+});
